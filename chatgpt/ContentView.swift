4480882dd59a354da5a9a54d87444ef61239aa61
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2023-10-28 16:52:42 +0300

    Initial Commit

---
+++ chatgpt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatgpt
+//
+//  Created by Phillip Jagger on 28.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatgpt/chatgptApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatgptApp.swift
+//  chatgpt
+//
+//  Created by Phillip Jagger on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct chatgptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
